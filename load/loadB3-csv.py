8a95279337b1a06f72114f311abc80b9de3c757b
Gabriel Caballeria <47371711+C4ball@users.noreply.github.com>
2019-05-06 22:55:48 -0300

    Add files via upload

--- load/loadB3-csv.py
+++ load/loadB3-csv.py
@@ -1,31 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
-bmf = open('COTAHIST_A2019.TXT','r')
 bmfload = open("bmf_load.csv",'w')
+bmfload.write('')
+bmfload.close()
 
-data_cotacao = []
-
-for l in bmf:
-
-    TIPREG = l[0:2]
-    DATAPREG = l[2:10]
-    CODNEG = l[12:24]
-    NOMRES = l[27:39]
-    MODREF = l[52:56]
-    PREABE = ((l[56:67]) + '.' + (l[67:69]))
-    PREMAX = ((l[69:80]) + '.' + (l[80:82]))
-    PREMIN = ((l[82:93]) + '.' + (l[93:95]))
-    PREMD = ((l[95:106]) + '.' + (l[106:108]))
-    PREULT = ((l[108:119]) + '.' + (l[119:121]))
-    PREOFC = ((l[121:132]) + '.' + (l[132:134]))
-    PREOFV = ((l[134:145]) + '.' + (l[145:147]))
-    CODISI = l[230:242]
+bmfload = open("bmf_load.csv",'a')
+for i in range(15,20):
+    bmf = open('COTAHIST_A20' + str(i) +'.TXT','r')
     
-    if TIPREG == "01":
+    data_cotacao = []
     
-        bmfload.write(TIPREG  + ',' + DATAPREG  + ',' + CODNEG  + ',' + NOMRES  + ',' + MODREF  + ',' + \
-              PREABE + ',' + PREMAX + ',' + PREMIN  + ',' + PREMD  + ',' + PREULT  + ',' + PREOFC  + ',' + PREOFV  + ',' + CODISI + "\n")
+    for l in bmf:
+    
+        TIPREG = l[0:2]
+        DATAPREG = l[2:10]
+        CODNEG = l[12:24]
+        NOMRES = l[27:39]
+        MODREF = l[52:56]
+        PREABE = ((l[56:67]) + '.' + (l[67:69]))
+        PREMAX = ((l[69:80]) + '.' + (l[80:82]))
+        PREMIN = ((l[82:93]) + '.' + (l[93:95]))
+        PREMD = ((l[95:106]) + '.' + (l[106:108]))
+        PREULT = ((l[108:119]) + '.' + (l[119:121]))
+        PREOFC = ((l[121:132]) + '.' + (l[132:134]))
+        PREOFV = ((l[134:145]) + '.' + (l[145:147]))
+        CODISI = l[230:242]
+        
+        if TIPREG == "01":
+        
+            bmfload.write(TIPREG  + ',' + DATAPREG  + ',' + CODNEG  + ',' + NOMRES  + ',' + MODREF  + ',' + \
+                  PREABE + ',' + PREMAX + ',' + PREMIN  + ',' + PREMD  + ',' + PREULT  + ',' + PREOFC  + ',' + PREOFV  + ',' + CODISI + "\n")
 
     
 bmfload.close()
