23679f5ad393a0f15e0d8ab90824203bc5a7f5d6
Rodrigo Dias <rodrigodds@outlook.com>
2019-05-21 19:27:46 -0300

    Update loadB3-postgresql.py

--- load/loadB3-postgresql.py
+++ load/loadB3-postgresql.py
@@ -2,7 +2,7 @@ import psycopg2
 conn = psycopg2.connect("host=127.0.0.1 dbname=projetotcc user=postgres password=123456")
 cur = conn.cursor()
 cur.execute("truncate dadosb3;")
-with open('/twitter/tccbi17/load/bmf_load.csv', 'r') as f:
+with open('/twitter/tccbi17/load/b3/bmf_load.csv', 'r') as f:
 # Notice that we don't need the `csv` module.
     #next(f) # Skip the header row.
     cur.copy_from(f, 'dadosb3', sep=',')
